33fe1d532786d12770e382b25394c3d362742b37
speedydelete <speedykorok777@gmail.com>
2025-08-02 15:49:33 -0400

    stuff 2 (incomplete)

--- src/pattern.ts
+++ src/pattern.ts
@@ -4,6 +4,8 @@ import {parseRule, Rule, transitionGridToNumber} from './rule.js';
 
 const RLE_CHARS = '.ABCDEFGHIJKLMNOPQRSTUVWX';
 const RLE_EXT_CHARS = 'pqrstuvxwy';
+const RLE_CHARS_ARRAY = Array.from(RLE_CHARS);
+const RLE_OUT_CHARS = RLE_CHARS_ARRAY.concat(Array.from(RLE_EXT_CHARS).flatMap(char => RLE_CHARS_ARRAY.map(x => char + x)));
 
 export class Pattern {
 
@@ -15,6 +17,8 @@ export class Pattern {
     period: number = -1;
     dx: number = -1;
     dy: number = -1;
+    absX: number = 0;
+    absY: number = 0;
 
     constructor(rule: string, data: number[][] = []) {
         this.data = data;
@@ -68,12 +72,14 @@ export class Pattern {
         }
         while (this.data.at(0)?.every(x => x === 0)) {
             this.data.shift();
+            this.absY++;
         }
         while (this.data.at(-1)?.every(x => x === 0)) {
             this.data.pop();
         }
         while (this.data.every(x => x[0] === 0)) {
             this.data.forEach(x => x.shift());
+            this.absX++;
         }
         while (this.data.every(x => x.at(-1) === 0)) {
             this.data.forEach(x => x.pop());
@@ -91,6 +97,8 @@ export class Pattern {
         this.data = newData;
         this.height += offsetY;
         this.width += offsetX;
+        this.absX -= offsetX;
+        this.absY -= offsetY;
         return this;
     }
 
@@ -101,10 +109,52 @@ export class Pattern {
         return this.parsedRule;
     }
 
+    rotateRight(): this {
+        this.data = Array.from({length: this.width}, (_, i) => this.data.map(row => row[i]));
+        [this.height, this.width] = [this.width, this.height];
+        this.data.forEach(row => row.reverse());
+        return this;
+    }
+
     run(): Pattern {
         let p = new Pattern(this.rule, this.data);
         let rule = this.getParsedRule();
         p.parsedRule = rule;
+        if (rule.r === 1) {
+            p.resize(
+                p.height + Number(p.data[p.data.length - 1].some(x => x !== 0)),
+                p.width + Number(p.data.some(x => x[x.length - 1] !== 0)),
+            );
+            p.offsetBy(
+                Number(p.data[0].some(x => x !== 0)),
+                Number(p.data.some(x => x[0] !== 0)),
+            );
+        } else {
+            let yOffset = 0;
+            let xOffset = 0;
+            let heightChange = 0;
+            let widthChange = 0;
+            for (let i = 0; i < rule.r; i++) {
+                if (p.data[i].some(x => x !== 0) && yOffset < rule.r - i) {
+                    yOffset = rule.r - i;
+                }
+                if (p.data.some(row => row[i] !== 0) && xOffset < rule.r - i) {
+                    xOffset = rule.r - i;
+                }
+                if (p.data[p.height - i - 1].some(x => x !== 0) && heightChange < i + 1) {
+                    heightChange = i + 1;
+                }
+                if (p.data.some(row => row[p.width - i - 1] !== 0) && widthChange < i + 1) {
+                    widthChange = i + 1;
+                }
+            }
+            if (heightChange || widthChange) {
+                p.resize(p.height + heightChange, p.width + widthChange);
+            }
+            if (xOffset || yOffset) {
+                p.offsetBy(xOffset, yOffset);
+            }
+        }
         let sets: [number, number, number][] = [];
         for (let y = 0; y < p.height; y++) {
             for (let x = 0; x < p.width; x++) {
@@ -202,8 +252,22 @@ export class Pattern {
         return p.resizeToFit();
     }
 
+    getPopulation(): number {
+        return this.data.reduce((x, row) => x + row.reduce((y, z) => y + z, 0), 0);
+    }
+
     identify(): this {
-        return this;
+        let p: Pattern = this;
+        for (let period = 1; period < 65536; period++) {
+            p = p.run();
+            if (this.data.every((x, i) => x.every((y, j) => y === p.data[i][j]))) {
+                this.period = period;
+                this.dx = p.absX - this.absX;
+                this.dy = p.absY - this.absY;
+                return this;
+            }
+        }
+        throw new Error('Cannot identify pattern');
     }
 
     static fromRLE(rle: string): Pattern {
@@ -260,6 +324,104 @@ export class Pattern {
             }
         }
         return out;
+    }
+
+    toRLE(): string {
+        let rows = this.data.map(x => {
+            let out = x.map(y => RLE_OUT_CHARS[y]);
+            while (out.at(-1) === 'b') {
+                out.pop();
+            }
+            return out;
+        });
+        while (rows.at(0)?.length === 0) {
+            rows.shift();
+        }
+        while (rows.at(1)?.length === 0) {
+            rows.pop()
+        }
+        let beforeRLE = rows.flatMap(x => {x.push('$'); return x;});
+        let out =  `x = ${this.width}, y = ${this.height}, rule = ${this.rule}\n`;
+        if (beforeRLE.length === 0) {
+            return out + '!';
+        }
+        let runLength = 1;
+        let runChar = beforeRLE[0];
+        for (let char of beforeRLE.slice(1)) {
+            if (runChar === char) {
+                runLength++;
+            } else {
+                if (runLength === 1) {
+                    out += runChar;
+                } else {
+                    out += runLength + runChar;
+                }
+                runChar = char;
+                runLength = 1;
+            }
+            if (out.lastIndexOf('\n') < -60) {
+                out += '\n';
+            }
+        }
+        if (runLength === 1) {
+            out += runChar;
+        } else {
+            out += runLength + runChar;
+        }
+        return out + '!\n';
+    }
+
+    _toFormat(): [string, [number, number], [number, number], number, number, string] {
+        this.identify();
+        let out: string;
+        if (this.dx === 0 && this.dy === 0) {
+            out = 'p' + this.period;
+        } else {
+            if (this.dx === 0 || this.dy === 0) {
+                while (this.dx <= 0 || this.dy !== 0) {
+                    this.rotateRight();
+                }
+                if (this.dx === 1) {
+                    out = 'c/' + this.period + 'o';
+                } else {
+                    out = this.dx + 'c/' + this.period + 'o';
+                }
+            } else {
+                while (this.dx < 0 || this.dy < 0) {
+                    this.rotateRight();
+                }
+                if (this.dx === this.dy) {
+                    if (this.dx === 1) {
+                        out = 'c/' + this.period + 'd';
+                    } else {
+                        out = this.dx + 'c/' + this.period + 'd';
+                    }
+                } else {
+                    out = `(${this.dx}, ${this.dy})c/${this.period}`;
+                }
+            }
+        }
+        let minBB: [number, number] = [this.width, this.height];
+        let maxBB: [number, number] = [-this.width, -this.height];
+        let minPop = this.getPopulation();
+        let maxPop = minPop;
+        let p: Pattern = this;
+        for (let i = 1; i < this.period; i++) {
+            p = p.run();
+            let bb = [this.width, this.height];
+            let pop = this.getPopulation();
+            if (pop < minPop) {
+                minPop = pop;
+            }
+            if (pop > maxPop) {
+                maxPop = pop;
+            }
+        }
+        return [out, minBB, maxBB, minPop, maxPop, this.toRLE().split('\n').slice(1).join('')];
+    }
+
+    toFormat(): [string, [number, number], [number, number], number, number, string] {
+        return (new Pattern(this.rule, this.data))._toFormat();
     }
 
 }
